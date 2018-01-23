2663f0c68ee52137588f5ca4e7ad2eedfc3e87fb
Shay Alexander <shaytopazehair@gmail.com>
2018-01-23 01:33:18 +0000

    created secrets file to hold token and added secrets file to gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,20 @@
 var request = require('request');
+var token = require('./secrets');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
-}
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+      // 'Authorization': '71396a230d610cf8cee5ccf65a8cebb70db73bee'
+
+    }
+  }
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+};
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
