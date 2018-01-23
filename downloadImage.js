376c4aee3c36b3af5aba65d20ee41e37bb194019
Shay Alexander <shaytopazehair@gmail.com>
2018-01-23 02:56:23 +0000

    created avatar directory to save avatar images into - and exported my downloadImageByURL Function to another file, and called my function from that file to check if it was working

---
+++ downloadImage.js
@@ -0,0 +1,3 @@
+var downloadImageByURL = require('./download_avatars');
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets');
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -26,3 +27,20 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("avatar.url: ", result[index].avatar_url);
 }
 });
+
+module.exports = function downloadImageByURL(url, filePath) {
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Downloading image...');
+         console.log('Response: ' + response.statusCode + ' Response Message: ' + response.statusMessage + ' Content Type: ' + response.headers['content-type']);
+       })
+       .on('end', function (end) {
+        console.log('Download complete');
+        })
+       .pipe(fs.createWriteStream(filePath));
+}
+
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
