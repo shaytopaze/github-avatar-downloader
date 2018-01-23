0a2ae807916cd2333a034a99320c7ede3631b1e0
Shay Alexander <shaytopazehair@gmail.com>
2018-01-23 03:58:48 +0000

    fixed syntax errors and added comments

--- download_avatars.js
+++ download_avatars.js
@@ -3,14 +3,13 @@ var secrets = require('./secrets');
 var fs = require('fs');
 
 function getRepoContributors(cb) {
-if (process.argv.length < 4) {
-  console.log('Not enough info... Need repoOwner AND repoName!');
-  return;
-}
+  if (process.argv.length < 4) { // if command line is given less than two arugments - throw this error
+    console.log('Not enough info... Need repoOwner AND repoName!');
+    return;
+  }
 
   var repoOwner = process.argv[2];
   var repoName = process.argv[3];
-
 
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -20,6 +19,7 @@ if (process.argv.length < 4) {
 
     }
   }
+
   request(options, function(err, res, body) {
     var bodyObject = JSON.parse(body);
     cb(err, bodyObject);
@@ -27,29 +27,29 @@ if (process.argv.length < 4) {
 };
 
 getRepoContributors( function(err, result) {
-  for (var index in result) {
+  for (var index in result) { // loop through result array
     if (err){
       console.log(err);
       return err;
     }
-    // console.log('\n');
-    // console.log("avatar.url: ", result[index].avatar_url);
-    downloadImageByURL(result[index].avatar_url, "./avatars/" + result[index].login + '.jpg')
-
-}
+    downloadImageByURL(result[index].avatar_url, "./avatars/" + result[index].login + '.jpg') // access avatar_url key and login key
+  }
 });
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
-         console.log('Downloading image...');
-         console.log('Response: ' + response.statusCode + ' Response Message: ' + response.statusMessage + ' Content Type: ' + response.headers['content-type']);
-       })
-       .on('end', function (end) {
-        console.log('Download complete');
-        })
-       .pipe(fs.createWriteStream(filePath));
+  .on('error', function (err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log('Downloading image...');
+    console.log('Response: ' + response.statusCode + ' Response Message: ' + response.statusMessage + ' Content Type: ' + response.headers['content-type']);
+  })
+  .on('end', function (end) {
+    console.log('Download complete');
+  })
+  .pipe(fs.createWriteStream(filePath));
 }
+
+
+
